f5d98e65785b22030fdcd9001afc772385344a6c
glaffamabili <glaffamabili@gmail.com>
2025-09-04 20:53:28 +0000

    mudanças aula 01/09/2025

---
+++ src/main/java/app/Tarefa.java
@@ -0,0 +1,21 @@
+package app;
+
+public class Tarefa extends Atividade{
+    
+    private boolean isConcluido;
+
+    
+    public boolean isConcluido(){
+        return isConcluido;
+    }
+
+    public void setConcluido(boolean isConcluido){
+        this.isConcluido = isConcluido;
+    }
+    
+    public Tarefa(long id, String descricao, boolean isConcluido){
+        this.setId(id);
+        this.setDescricao(descricao);
+        this.setConcluido(isConcluido);
+    }
+}
